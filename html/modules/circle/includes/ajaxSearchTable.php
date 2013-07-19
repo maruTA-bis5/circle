2ca6ca4855b152d7ab56dfc0c46cc6a99aa27020
bis5 <bis5@bis5.mydns.jp>
2013-07-19 16:03:51 +0900

    create new file(skelton)

---
+++ html/modules/circle/includes/ajaxSearchTable.php
@@ -0,0 +1,5 @@
+<?php
+
+require_once '../../../mainfile.php';
+
+require_once XOOPS_TRUST_PATH . '/modules/circle/includes/ajaxSearchTable.php';
---
+++ xoops_trust_path/modules/circle/includes/ajaxSearchTable.php
@@ -0,0 +1,6 @@
+<?php
+
+header('Content-Type: text/html; charset=UTF-8');
+
+require_once XOOPS_ROOT_PATH . '/header.php';
+?>
